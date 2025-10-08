4284105aa10bff1399d780b74e6b3cc56e2bd2ab
innaturllc <innaturllc@gmail.com>
2025-10-08 12:30:35 +0700

    8.10.2025_6

--- next.config.js
+++ next.config.js
@@ -1,3 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 module.exports = nextConfig;
+
+// next.config.js
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: "/paywall/:path*",
+        destination: "https://flexy-pilates.superwall.app/:path*", // external URL
+      },
+    ];
+  },
+  // (optional) if Superwall sets CSP or needs forwarded headers:
+  async headers() {
+    return [
+      {
+        source: "/paywall/:path*",
+        headers: [
+          { key: "X-Forwarded-Host", value: "thnksflexypage.vercel.app" },
+          { key: "X-Forwarded-Proto", value: "https" },
+        ],
+      },
+    ];
+  },
+};
